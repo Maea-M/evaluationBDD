544db397f1c041d740f3871bef0d733727944ccc
Morgane Mahé <maeamahe@gmail.com>
2023-01-15 17:17:50 +0100

    errorSql #1005 Errcode:FKconstraint is incorrectly

--- complex_cinema.sql
+++ complex_cinema.sql
@@ -4,9 +4,27 @@ CREATE DATABASE IF NOT EXISTS complex_cinema CHARACTER SET UTF8mb4 COLLATE utf8m
 /*Create table customers*/
 CREATE TABLE IF NOT EXISTS customers
 (
-    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    id INT (11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     lastname VARCHAR(30) NOT NULL,
     firstname VARCHAR(30) NOT NULL,
     password VARCHAR(60) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL
+    email VARCHAR(255) UNIQUE NOT NULL,
+    role VARCHAR (20) NOT NULL
+) ENGINE = InnoDB;
+
+/*Create table role*/
+CREATE TABLE IF NOT EXISTS roles
+(
+    id INT (11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name_role VARCHAR (30) NOT NULL
+) ENGINE = InnoDB;
+
+/*Create table user_role*/
+CREATE TABLE IF NOT EXISTS user_role
+(
+    customersID CHAR(36) NOT NULL,
+    roleId INT(11) NOT NULL,
+    PRIMARY KEY (customersId, roleId),
+    FOREIGN KEY (customersId) REFERENCES customers(id),
+    FOREIGN KEY (roleId) REFERENCES roles(id)
 ) ENGINE = InnoDB;
