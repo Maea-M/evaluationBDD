39ba4b528b74224b0cb896164f1d527ae9b58512
Morgane Mahé <maeamahe@gmail.com>
2023-01-18 13:39:14 +0100

    tabls created^^

--- complex_cinema.sql
+++ complex_cinema.sql
@@ -41,8 +41,7 @@ CREATE TABLE IF NOT EXISTS complex
     phone INT(10) NOT NULL,
     number_room INT(20) NOT NULL,
     manager_id INT DEFAULT 1,
-    CONSTRAINT `erreur_manager`
-    FOREIGN KEY (manager_id) REFERENCES customers(id) ON DELETE SET NULL ON UPDATE RESTRICT
+    CONSTRAINT `fk_erreur_manager_id` FOREIGN KEY (`manager_id`) REFERENCES `customers`(`id`) ON DELETE SET NULL ON UPDATE RESTRICT
 ) ENGINE = InnoDB;
 
 /* Create table rooms */
@@ -52,7 +51,7 @@ CREATE TABLE IF NOT EXISTS rooms
     name_room VARCHAR(50) NOT NULL,
     max_places INT(255) NOT NULL,
     complex_id INT DEFAULT 1,
-    CONSTRAINT `erreur_complex` FOREIGN KEY (complex_id) REFERENCES complex(id) ON DELETE CASCADE
+    CONSTRAINT `erreur_complex` FOREIGN KEY (`complex_id`) REFERENCES `complex`(`id`) ON DELETE CASCADE
 ) ENGINE = InnoDB;
 
 /* Create table movie*/
@@ -64,24 +63,35 @@ CREATE TABLE IF NOT EXISTS movie
     time DATETIME NOT NULL
 ) ENGINE = InnoDB;
 
+/* Create table Price */
+CREATE TABLE IF NOT EXISTS price
+(
+    id INT (11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    type VARCHAR(50) NOT NULL,
+    amount DECIMAL (10,2) NOT NULL
+) ENGINE = InnoDB;
+
 /* Create table session */
 CREATE TABLE IF NOT EXISTS sessions
 (
+    id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     start_date DATETIME NOT NULL,
     end_date DATETIME NOT NULL,
     movie_id INT (11),
     rooms_id INT (11),
-    PRIMARY KEY (movie_id, rooms_id),
+    price_id INT (11),
     CONSTRAINT `fk_movie_id` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`id`) ON DELETE CASCADE,
-    CONSTRAINT `fk_rooms_id` FOREIGN KEY (`rooms_id`) REFERENCES `rooms` (`id`) ON DELETE CASCADE
+    CONSTRAINT `fk_rooms_id` FOREIGN KEY (`rooms_id`) REFERENCES `rooms` (`id`) ON DELETE CASCADE,
+    CONSTRAINT `fk_price_id` FOREIGN KEY (`price_id`) REFERENCES `price` (`id`) ON DELETE CASCADE
 ) ENGINE = InnoDB;
 
 /* Create table booking */
 CREATE TABLE IF NOT EXISTS booking
 (
-    session_id INT (11),
+    sessions_id INT (11),
     customers_id INT (11),
-    PRIMARY KEY (session_id, customers_id),
-    CONSTRAINT `fk_session_id` FOREIGN KEY (`session_id`) REFERENCES `session` (`id`) ON DELETE CASCADE,
-    CONSTRAINT `fk_customers_id` FOREIGN KEY (`customers_id`) REFERENCES `customers` (`id`) ON DELETE CASCADE    
-) ENGINE = InnoDB;
+    CONSTRAINT `pk_booking` PRIMARY KEY (`sessions_id`, `customers_id`),
+    FOREIGN KEY (`sessions_id`) REFERENCES `sessions`(`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`customers_id`) REFERENCES `customers`(`id`) ON DELETE CASCADE,
+    UNIQUE (sessions_id, customers_id) 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
